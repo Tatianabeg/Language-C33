9b408fb5ccb690aa49b80b9cb9dbda94efe6f680
Tatiana <tatiana.beglarova@gmail.com>
2022-10-06 19:28:57 +0300

    list

--- lec.py
+++ lec.py
@@ -1,81 +1,58 @@
-# Напишите программу, которая принимает на вход вещественное число и 
-# показывает сумму его цифр.
+# Напишите программу, которая найдёт сумму элементов списка, стоящих на нечётной позиции. Пример:
 
-num = input("Enter the number: ")
-sum = 0
-for i in num:
-    if i!=".":
-        sum = sum + int(i)
-print(f"The sum of the {num} is: ", sum)
+# [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12
 
-# Напишите программу, которая
-# принимает на вход число N и выдает набор произведений чисел от 1 до N.
-
-
-def factorial (number, count = 1):
-    for i in range(1, number + 1):
-        count *= i
-    return count
-
-n = int(input('Enter the number: '))
-print(f'Set of products of numbers from 1 to {n} = ', end = '')
-for i in range(1, n + 1):
-    if i == n: 
-        print(f'{factorial(i)}')
-    else:
-        print(f'{factorial(i)}', end = ', ')
-
-# Задайте список из n чисел 
-# последовательности $(1+\frac 1 n)^n$ и выведите на экран их сумму.
-
-n = int(input('Enter number: ')) 
-
-def sequence(n):
-
-    return[round((1 + 1 / x)**x, 2) for x in range (1, n + 1)]   
-        
-print(sequence(n))
-print(round(sum(sequence(n))))
-
-
-# Задайте список из N элементов, заполненных числами из промежутка [-N, N]. 
-# Найдите произведение элементов на указанных позициях. 
-
-from random import randint
-numbers = []
-for i in range(10):
-    numbers.append(randint (-10,10))
-print(numbers)
-
-def get_numbers(numbers):
-    count =0 
-    for element in numbers:
-        count +=1
-    return count
-print('Number of elements: ', get_numbers(numbers))
-
-x = int(input('Enter  position of first element: '))
-y = int(input('Enter position of second element: '))
-
-for i in range(len(numbers)):
-    mult = numbers[x -1]*numbers[y - 1]
-print(f'Mult of elements: {numbers[x -1]} * {numbers[y -1]} =', mult)
-
-# Реализуйте алгоритм перемешивания списка.
-
-import random
-def mix_list(list_original):
-    list = list_original[:]
-    list_length = len(list)
-    for i in range(list_length):
-        index_aleatory = random.randint(0, list_length - 1)
-        temp = list[i]
-        list[i] = list[index_aleatory]
-        list[index_aleatory] = temp
-    return list
-list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-mixed_list = mix_list(list)
-print("Primary list: ")
+list = [2, 3, 5, 9, 3]
 print(list)
-print("Mixed list: ")
-print(mixed_list)
+
+sum = 0
+for i in range(len(list)):
+    if i %  2 != 0:
+        sum = sum + list[i]
+print(f'Sum of elements in odd position {list} is', sum)
+
+# Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+# Пример:
+# - [2, 3, 4, 5, 6] => [12, 15, 16];
+# - [2, 3, 5, 6] => [12, 15]
+
+list = [2,3,4,5,6]
+print(list,'==>')
+
+def mult(list):
+    mult = []
+    for i in range((len(list)+1)//2):
+        mult.append(list[i]*list[len(list)-1-i])
+    return mult
+print(mult(list))
+
+
+# Напишите программу, которая найдёт разницу между
+# максимальным и минимальным значением дробной части элементов.
+# Пример:
+# - [1.1, 1.2, 3.1, 5, 10.01] => 0.19
+
+list = [1.1, 1.2, 3.1, 5, 10.01]
+print(list)
+
+def dif(list):
+    dif_max_min =[]
+    for i in range(len(list)):
+        dif_max_min.append(list[i]%1)
+    return max(dif_max_min) - min(dif_max_min)
+print(round(dif(list),2))
+
+
+# Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+# Пример
+
+# - 45 -> 101101
+# - 3 -> 11
+# - 2 -> 10
+
+a = int(input('Enter the number: '))
+b = ''
+while a > 0:
+    b = str(a % 2)+b
+    a = a//2
+print(b)
